e89c5ca9959feeec1b610144c2905a5bf8e5b7e0
SabrinaSimao <sabrina_simao@hotmail.com>
2017-10-18 13:33:37 -0200

    new stuff

---
+++ Telefone/decoderDTMF.py
@@ -0,0 +1,205 @@
+import sounddevice as sd
+import matplotlib.pyplot as plt
+import time
+import numpy as np
+import scipy
+import soundfile as sf
+import math
+import fourier
+
+class decoderDTMF(object):
+
+	def __init__(self):
+
+		self.fs = 44100
+		self.erro = 30 # valor que é admitido como erro da frequencia na hora de receber a onda
+		self.loops = 4 
+
+	def decode(self):
+
+		som,Fourier_Transform = self.time_plot(self.loops)
+
+		#som,Fourier_Transform = self.forever_plot()
+
+		self.salva_wav(som,'Sound_received.wav') # salva os som
+
+		print(Fourier_Transform)
+
+
+
+	def salva(self,som,nome_do_arquivo):
+
+		print("salvando")
+		thefile = open(nome_do_arquivo, 'w')
+		for item in som:
+		  thefile.write("%s\n" % item)
+		thefile.close()
+
+	def salva_wav(self,som,nome_do_arquivo):
+		print("salvando som em: ",nome_do_arquivo)
+		sf.write(nome_do_arquivo, som, self.fs)
+
+
+	def acha_tecla(self,frq1,frq2):
+		#primera coluna da tabela
+		if 1209 - self.erro <= frq1 <= 1209 + self.erro:
+			if 697 - self.erro <= frq2 <= 697 + self.erro:
+				return "1"
+			if 770 - self.erro <= frq2 <= 770 + self.erro:
+				return "4"
+			if 852 - self.erro <= frq2 <= 852 + self.erro:
+				return "7"
+			if 941 - self.erro <= frq2 <= 941 + self.erro:
+				return "*"
+
+		#segunda
+		if 1336 - self.erro <= frq1 <= 1336 + self.erro:
+			if 697 - self.erro <= frq2 <= 697 + self.erro:
+				return "2"
+			if 770 - self.erro <= frq2 <= 770 + self.erro:
+				return "5"
+			if 852 - self.erro <= frq2 <= 852 + self.erro:
+				return "8"
+			if 941 - self.erro <= frq2 <= 941 + self.erro:
+				return "0"
+
+
+		#terceira
+		if 1477 - self.erro <= frq1 <= 1477 + self.erro:
+			if 697 - self.erro <= frq2 <= 697 + self.erro:
+				return "3"
+			if 770 - self.erro <= frq2 <= 770 + self.erro:
+				return "6"
+			if 852 - self.erro <= frq2 <= 852 + self.erro:
+				return "9"
+			if 941 - self.erro <= frq2 <= 941 + self.erro:
+				return "#"
+
+		else:
+			return "nenhuma tecla encontrada"
+
+
+
+
+
+	def time_plot(self,duração):
+		plt.ion()
+		lista =[]
+
+		for tempo in range(duração):
+			print("loop:",tempo)
+			audio = sd.rec(int(1*self.fs), self.fs, channels=1)
+			sd.wait()
+
+			som = audio[:,0]
+			som = som[9000:] # retira o começo da gravação que sempre esta errada
+
+			t = np.linspace(0,1,1*self.fs)
+			t = t[9000:] # retira o começo da gravação que sempre esta errada
+
+			plt.clf()
+
+			#calcula fourier
+			X,Y = fourier.calcFFT(som, self.fs)
+			Y2 = Y
+			y_lista = Y2.tolist()
+			
+			for i in range(len(y_lista)):
+				i_db = 10*(math.log10(np.abs(y_lista[i])/25000))
+				y_lista[i] = i_db
+			
+			#plota fourier
+			plt.figure("db")
+			plt.plot(X,y_lista)
+			plt.xlim(0, 2000)
+			plt.ylabel('decibeis')
+			plt.xlabel('hertz')
+			plt.grid()
+			plt.title('Decibeis por Hz')
+
+			
+			plt.pause(1)
+
+			#plt.figure(figsize=(10, 10))
+			plt.plot(t,som)
+			plt.xlabel('tempo')
+			plt.ylabel('sin(t) recebido')
+			#plt.ylim(-0.55,0.55)
+
+			plt.pause(1)
+			plt.close()
+
+			frq1,frq2 = fourier.acha_maximos(X,Y)
+			print("Tecla telefonica acionada: ",self.acha_tecla(frq1,frq2))
+
+
+			lista = np.concatenate([lista,som])
+
+
+		plt.close()
+
+		print(lista)
+		Fourier_Transform = scipy.fft(lista)
+		#plt.plot(Fourier_Transform)
+		time.sleep(10)
+		return (lista, Fourier_Transform)
+
+	def forever_plot(self): # mesma coisa do anterior mas esse roda até crt-c for precionado
+		plt.ion()
+		lista =[]
+
+		try:
+			while True:
+
+				audio = sd.rec(int(1*self.fs), self.fs, channels=1)
+				sd.wait()
+
+				som = audio[:,0]
+				som = som[9000:] # retira o começo da gravação que sempre esta errada
+
+				t = np.linspace(0,1,1*self.fs)
+				t = t[9000:] # retira o começo da gravação que sempre esta errada
+
+				plt.clf()
+
+				#calcula fourier
+				X,Y = fourier.calcFFT(som, self.fs)
+
+				
+				#plota fourier
+				plt.figure("abs(Y[k])")
+				plt.plot(X,np.abs(Y))
+
+				plt.xlabel('')
+				plt.ylabel('')
+				plt.grid()
+				plt.title('Modulo Fourier audio')
+
+				
+				plt.pause(1)
+
+
+				lista = np.concatenate([lista,som])
+		except KeyboardInterrupt:
+			pass
+
+
+		plt.close()
+
+		print(lista)
+		Fourier_Transform = scipy.fft(lista)
+		#plt.plot(Fourier_Transform)
+		time.sleep(10)
+		return (lista, Fourier_Transform)
+
+
+
+
+	def reproduz(self,som):
+		print("reprodução:")
+		print(som)
+		sd.play(som, self.fs)
+		sd.wait()
+
+
+decoderDTMF().decode()
---
+++ Telefone/encoderDTMF.py
@@ -0,0 +1,129 @@
+import tkinter as tk
+import numpy as np
+import math
+import sender as snd
+import scipy
+
+
+class Main():
+
+    def iniciar(self):
+        self.window.mainloop()
+#variaveis
+    fs = 44100
+    tempo = 1
+    t = np.linspace(0,tempo,fs*tempo)
+
+    y1 = np.sin(2*math.pi*697*t)
+    y2 = np.sin(2*math.pi*770*t)
+    y3 = np.sin(2*math.pi*852*t)
+    y4 = np.sin(2*math.pi*941*t)
+    x1 = np.sin(2*math.pi*1209*t)
+    x2 = np.sin(2*math.pi*1336*t)
+    x3 = np.sin(2*math.pi*1477*t)
+    x4 = np.sin(2*math.pi*1633*t)
+
+    t1 = y1 + x1
+    t2 = y1 + x2
+    t3 = y1 + x3
+    t4 = y2 + x1
+    t5 = y2 + x2
+    t6 = y2 + x3
+    t7 = y3 + x1
+    t8 = y3 + x2
+    t9 = y3 + x3
+    t0 = y4 + x2
+#defs de enviar audios e plotar
+    def T1(self):
+        snd.Telefone(Main.t1)
+        snd.Fourier(Main.t1)
+        #print("Tranformada de fourier: ",scipy.fft(Main.t1))
+    def T2(self):
+        snd.Telefone(Main.t2)
+        snd.Fourier(Main.t2)
+        #print("Tranformada de fourier: ",scipy.fft(Main.t2))
+    def T3(self):
+        snd.Telefone(Main.t3)
+        snd.Fourier(Main.t3)
+        #print("Tranformada de fourier: ",scipy.fft(Main.t3))
+    def T4(self):
+        snd.Telefone(Main.t4)
+        snd.Fourier(Main.t4)
+        #print("Tranformada de fourier: ",scipy.fft(Main.t4))
+    def T5(self):
+        snd.Telefone(Main.t5)
+        snd.Fourier(Main.t5)
+        #print("Tranformada de fourier: ",scipy.fft(Main.t5))
+    def T6(self):
+        snd.Telefone(Main.t6)
+        snd.Fourier(Main.t6)
+        #print("Tranformada de fourier: ",scipy.fft(Main.t6))
+    def T7(self):
+        snd.Telefone(Main.t7)
+        snd.Fourier(Main.t7)
+        #print("Tranformada de fourier: ",scipy.fft(Main.t7))
+    def T8(self):
+        snd.Telefone(Main.t8)
+        snd.Fourier(Main.t8)
+        #print("Tranformada de fourier: ",scipy.fft(Main.t8))
+    def T9(self):
+        snd.Telefone(Main.t9)
+        snd.Fourier(Main.t9)
+        #print("Tranformada de fourier: ",scipy.fft(Main.t9))
+    def T0(self):
+        snd.Telefone(Main.t0)
+        snd.Fourier(Main.t0)
+        #print("Tranformada de fourier: ",scipy.fft(Main.t0))
+
+#interface
+    def __init__(self):
+
+        height = 3
+        width = 3
+      
+        self.window = tk.Tk()
+        self.window.geometry("60x250")
+     
+        self.button1 = tk.Button(self.window, text = "1", height = height, width = width)
+        self.button1.grid(row = 0, column = 0)
+        self.button1.configure(command = self.T1)
+        #self.button1.bind("<button0>",1)
+
+        self.button2 = tk.Button(self.window, text = "2", height = height, width = width)
+        self.button2.grid(row = 0, column =1)
+        self.button2.configure(command = self.T2)
+
+        self.button3 = tk.Button(self.window, text = "3", height = height, width = width)
+        self.button3.grid(row = 0, column = 2)
+        self.button3.configure(command = self.T3)
+
+        self.button4 = tk.Button(self.window, text = "4", height = height, width = width)
+        self.button4.grid(row = 1, column = 0)
+        self.button4.configure(command = self.T4)
+
+        self.button5 = tk.Button(self.window, text = "5", height = height, width = width)
+        self.button5.grid(row = 1, column = 1)
+        self.button5.configure(command = self.T5)
+
+        self.button6 = tk.Button(self.window, text = "6", height = height, width = width)
+        self.button6.grid(row = 1, column = 2)
+        self.button6.configure(command = self.T6)
+
+        self.button7 = tk.Button(self.window, text = "7", height = height, width = width)
+        self.button7.grid(row = 2, column = 0)
+        self.button7.configure(command = self.T7)
+        
+        self.button8 = tk.Button(self.window, text = "8", height = height, width = width)
+        self.button8.grid(row = 2, column = 1)
+        self.button8.configure(command = self.T8)
+        
+        self.button9 = tk.Button(self.window, text = "9", height = height, width = width)
+        self.button9.grid(row = 2, column = 2)
+        self.button9.configure(command = self.T9)
+        
+        self.button0 = tk.Button(self.window, text = "0", height = height, width = width)
+        self.button0.grid(row = 3, column = 1)
+        self.button0.configure(command = self.T0)
+        #self.button0.bind("<button0>",0)
+    
+Main().iniciar()
