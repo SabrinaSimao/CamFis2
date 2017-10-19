62bc26406e451d946c27a37b3de2f7393a363f55
SabrinaSimao <sabrina_simao@hotmail.com>
2017-10-19 11:56:47 -0200

    Fiz transmissor

--- transmissor.py
+++ transmissor.py
@@ -9,6 +9,19 @@ import math
 
 class transmissor(object):
 
+	def __init__(self):
+		self.fc1 = 500 #frequencia escolhida arbitrariamente
+		self.fc2 = 800 #frequencia escolhida arbitrariamente
+		self.ac1 = 100
+		self.ac2 = 200
+		self.fs = 44100
+		self.tempo1 = 4
+		self.tempo2 = 3
+		self.t1 = np.linspace(0,1,self.fs*self.tempo1)
+		self.t2 = np.linspace(0,1,self.fs*self.tempo2)
+		self.fp1 = self.ac1*np.sin(2*math.pi*self.fc1*self.t1)
+		self.fp2 = self.ac2*np.sin(2*math.pi*self.fc2*self.t2)
+
 	def transmitir(self):
 		m1, fs1 = sf.read('m1.wav')
 		#m1 = m1[:,1]
@@ -16,20 +29,47 @@ class transmissor(object):
 		m2, fs2 = sf.read('m2.wav')
 		#m2 = m2[:,1]
 
-		m1_corte = self.LPF(m1, 4000, fs1)
-		m2_corte = self.LPF(m2, 4000, fs2)
+		m1f = self.LPF(m1, 4000, fs1)
+		m2f = self.LPF(m2, 4000, fs2)
 
+		#self.plot_fourier(m1f,fs1)
+		#self.plot_fourier(m2f,fs2)
 
-		self.plot_fourier(m1,fs1)
-		self.plot_fourier(m2,fs2)
+		#self.reproduz(m1f,fs1)
+		#self.reproduz(m2f,fs2)
+	
+		print("Modulacoes: ")
+	
+		am1 = self.modula1(m1f)
+		am2 = self.modula2(m2f)
+	
+		# self.plot_fourier(self.fp1, fs1)
+		# self.plot_fourier(am1, fs1)
 
-		self.reproduz(m1_corte,fs1)
-		self.reproduz(m2_corte,fs2)
+		# self.plot_fourier(self.fp2, fs2)
+		# self.plot_fourier(am2, fs2)
+		
+		self.reproduz(am1, fs1)
+		self.reproduz(am2, fs2)
+		zero = np.zeros(44100) 
+		#Tempo dos audios é diferente, para somar eles, adicionamos 1 segundo de informacao cheia de zeros
+		am2_new = np.append(am2, zero)
+		soma = am1 + am2_new #somamos ambos sinais modulados
 
+		self.plot_fourier(soma, 44100)
+		self.reproduz(soma, 44100)
+	
+	def modula1(self,som):
+		am = som*self.fp1
+		return am
+
+	def modula2(self,som):
+		am = som*self.fp2
+		return am
 
 	def reproduz(self,som,fs):
-		print("reprodução:")
-		print(som)
+		print("reprodução...")
+		#print(som)
 		sd.play(som, fs)
 		sd.wait()
 
@@ -52,8 +92,7 @@ class transmissor(object):
 		plt.grid()
 		plt.title('Decibeis por Hz')
 
-		plt.pause(2)
-
+		plt.pause(1)
 
 
 	def LPF(self,signal, cutoff_hz, fs):
